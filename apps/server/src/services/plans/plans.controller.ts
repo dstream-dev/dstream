86bd8add657f4f27d52c3c5c43fbcdc43de6bb72
Vivek Chauhan <vivek.agent47@gmail.com>
2023-06-01 16:46:02 +0530

    CRUD for plans

--- apps/server/src/services/plans/plans.controller.ts
+++ apps/server/src/services/plans/plans.controller.ts
@@ -1,19 +1,27 @@
 import {
   Get,
+  Put,
   Body,
   Post,
+  Param,
+  Delete,
   Headers,
   UseGuards,
   Controller,
   HttpStatus,
   HttpException,
-  Param,
-  Put,
 } from "@nestjs/common";
 import { PlansService } from "./plans.service";
-import { Roles, RolesGuard, UserAuthGuard } from "src/utils";
+import {
+  Roles,
+  AuthUser,
+  RolesGuard,
+  UserAuthGuard,
+  IAuthUserDecorator,
+} from "src/utils";
 import { UserRole } from "src/entities";
 import { PlanDTO } from "src/dtos";
+import { activityLog } from "src/utils/activityLog";
 
 @Controller("plan")
 export class PlansController {
@@ -56,9 +64,23 @@ export class PlansController {
   async create(
     @Headers("x-organization-id") org_id: string,
     @Body() createPlanDto: PlanDTO,
+    @AuthUser() user: IAuthUserDecorator,
   ) {
     try {
-      return await this.plansService.create({ org_id, data: createPlanDto });
+      const newPlan = await this.plansService.create({
+        org_id,
+        data: createPlanDto,
+      });
+
+      activityLog({
+        org_id: org_id,
+        by: user.email,
+        activity_type: "plan",
+        activity_id: newPlan.id,
+        activity: "new plan created",
+      });
+
+      return newPlan;
     } catch (err) {
       throw new HttpException(
         err.message,
@@ -67,25 +89,64 @@ export class PlansController {
     }
   }
 
-  // @Roles(UserRole.OWNER, UserRole.ADMIN)
-  // @UseGuards(UserAuthGuard, RolesGuard)
-  // @Put(":id")
-  // async update(
-  //   @Headers("x-organization-id") org_id: string,
-  //   @Param("id") id: string,
-  //   @Body() updatePlanDto: PlanDTO,
-  // ) {
-  //   try {
-  //     return await this.plansService.update({
-  //       org_id,
-  //       id,
-  //       data: updatePlanDto,
-  //     });
-  //   } catch (err) {
-  //     throw new HttpException(
-  //       err.message,
-  //       err.status || HttpStatus.BAD_REQUEST,
-  //     );
-  //   }
-  // }
+  @Roles(UserRole.OWNER, UserRole.ADMIN)
+  @UseGuards(UserAuthGuard, RolesGuard)
+  @Put(":id")
+  async update(
+    @Headers("x-organization-id") org_id: string,
+    @Param("id") id: string,
+    @Body() updatePlanDto: PlanDTO,
+    @AuthUser() user: IAuthUserDecorator,
+  ) {
+    try {
+      const updatedPlan = await this.plansService.update({
+        org_id,
+        id,
+        data: updatePlanDto,
+      });
+
+      activityLog({
+        org_id: org_id,
+        by: user.email,
+        activity_type: "plan",
+        activity_id: id,
+        activity: "plan updated",
+      });
+
+      return updatedPlan;
+    } catch (err) {
+      throw new HttpException(
+        err.message,
+        err.status || HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Roles(UserRole.OWNER, UserRole.ADMIN)
+  @UseGuards(UserAuthGuard, RolesGuard)
+  @Delete(":id")
+  async remove(
+    @Headers("x-organization-id") org_id: string,
+    @Param("id") id: string,
+    @AuthUser() user: IAuthUserDecorator,
+  ) {
+    try {
+      const removeStatus = await this.plansService.delete({ org_id, id });
+
+      activityLog({
+        org_id: org_id,
+        by: user.email,
+        activity_type: "plan",
+        activity_id: id,
+        activity: "plan deleted",
+      });
+
+      return removeStatus;
+    } catch (err) {
+      throw new HttpException(
+        err.message,
+        err.status || HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
 }
--- apps/server/src/services/plans/plans.service.ts
+++ apps/server/src/services/plans/plans.service.ts
@@ -20,7 +20,7 @@ export class PlansService {
     try {
       return await this.planRepository
         .createQueryBuilder("plan")
-        .where("plan.org_id = :org_id", { org_id: string })
+        .where("plan.organization_id = :org_id", { org_id: string })
         .getMany();
     } catch (err) {
       throw new HttpException(
@@ -32,9 +32,17 @@ export class PlansService {
 
   async findOne({ org_id, id }: { org_id: string; id: string }) {
     try {
-      return await this.planRepository.findOne({
-        where: { organization_id: org_id, id },
-      });
+      return await this.planRepository
+        .createQueryBuilder("plan")
+        .innerJoinAndMapMany(
+          "plan.charges",
+          PlanCharges,
+          "plan_charges",
+          "plan_charges.plan_id = plan.id",
+        )
+        .where("plan.organization_id = :org_id", { org_id })
+        .andWhere("plan.id = :id", { id })
+        .getOne();
     } catch (err) {
       throw new HttpException(
         err.message,
@@ -72,7 +80,7 @@ export class PlansService {
           });
 
           for (let i = 0; i < data.charges.length; i += 1) {
-            await this.planChargesRepository.save({
+            await manager.save(PlanCharges, {
               id: createID("plan_charges"),
               plan_id: plan.id,
               organization_id: org_id,
@@ -98,58 +106,93 @@ export class PlansService {
     }
   }
 
-  // async update({
-  //   org_id,
-  //   id,
-  //   data,
-  // }: {
-  //   org_id: string;
-  //   id: string;
-  //   data: PlanDTO;
-  // }) {
-  //   try {
-  //     const plan = await this.findOne({ org_id, id });
-  //     if (!plan) {
-  //       throw new HttpException("Plan not found", HttpStatus.BAD_REQUEST);
-  //     }
+  async update({
+    org_id,
+    id,
+    data,
+  }: {
+    org_id: string;
+    id: string;
+    data: PlanDTO;
+  }) {
+    try {
+      const plan = await this.findOne({ org_id, id });
+      if (!plan) {
+        throw new HttpException("Plan not found", HttpStatus.BAD_REQUEST);
+      }
 
-  //     await this.planRepository.update(
-  //       { organization_id: org_id, id },
-  //       {
-  //         name: data.name,
-  //         description: data.description,
-  //         currency: data.currency,
-  //         external_plan_id: data.external_plan_id,
-  //         payment_term: data.payment_term,
-  //         min_charges: {
-  //           ...data.min_charges,
-  //         },
-  //       },
-  //     );
+      await this.entityManager.transaction(async (manager) => {
+        await manager.update(
+          Plan,
+          { organization_id: org_id, id },
+          {
+            name: data.name,
+            description: data.description,
+            currency: data.currency,
+            external_plan_id: data.external_plan_id,
+            payment_term: data.payment_term,
+            min_charges: {
+              ...data.min_charges,
+            },
+          },
+        );
 
-  //     await this.planChargesRepository.delete({
-  //       organization_id: org_id,
-  //       plan_id: id,
-  //     });
+        await manager.delete(PlanCharges, {
+          organization_id: org_id,
+          plan_id: id,
+        });
 
-  //     for (let i = 0; i < data.charges.length; i += 1) {
-  //       await this.planChargesRepository.save({
-  //         id: createID("plan_charges"),
-  //         plan_id: id,
-  //         organization_id: org_id,
-  //         metric_id: data.charges[i].metric_id,
-  //         cadence: data.charges[i].cadence,
-  //         active_min_charge: data.charges[i].active_min_charge,
-  //         min_charge: data.charges[i].min_charge,
-  //         pricing_model: data.charges[i].pricing_model,
-  //         pricing_scheme: data.charges[i].pricing_scheme,
-  //       });
-  //     }
-  //   } catch (err) {
-  //     throw new HttpException(
-  //       err.message,
-  //       err.status || HttpStatus.BAD_REQUEST,
-  //     );
-  //   }
-  // }
+        for (let i = 0; i < data.charges.length; i += 1) {
+          await manager.save(PlanCharges, {
+            id: createID("plan_charges"),
+            plan_id: id,
+            organization_id: org_id,
+            metric_id: data.charges[i].metric_id,
+            cadence: data.charges[i].cadence,
+            active_min_charge: data.charges[i].active_min_charge,
+            min_charge: data.charges[i].min_charge,
+            pricing_model: data.charges[i].pricing_model,
+            pricing_scheme: data.charges[i].pricing_scheme,
+          });
+        }
+      });
+
+      return {
+        status: HttpStatus.OK,
+        message: "Plan updated successfully",
+      };
+    } catch (err) {
+      throw new HttpException(
+        err.message,
+        err.status || HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async delete({ org_id, id }: { org_id: string; id: string }) {
+    try {
+      const plan = await this.findOne({ org_id, id });
+      if (!plan) {
+        throw new HttpException("Plan not found", HttpStatus.BAD_REQUEST);
+      }
+
+      await this.entityManager.transaction(async (manager) => {
+        await manager.delete(Plan, { organization_id: org_id, id });
+        await manager.delete(PlanCharges, {
+          organization_id: org_id,
+          plan_id: id,
+        });
+      });
+
+      return {
+        status: HttpStatus.OK,
+        message: "Plan deleted successfully",
+      };
+    } catch (err) {
+      throw new HttpException(
+        err.message,
+        err.status || HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
 }
